6c05d2c0df0b801c560fb25f94f3c93ebd8a6eac
Apit <apit@thezatapp.com>
2022-04-26 21:41:23 +0700

    refractor post_page

--- lib/list/screens/post_page.dart
+++ lib/list/screens/post_page.dart
@@ -1,9 +1,5 @@
-import 'package:aorl_apps_bloc/list/bloc/post_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-// import 'components/post_body.dart';
-import 'components/post_list.dart';
+import 'components/post_body.dart';
 
 class PostPage extends StatelessWidget {
   const PostPage({Key? key}) : super(key: key);
@@ -14,84 +10,7 @@ class PostPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Post Page'),
       ),
-      body: BlocProvider(
-        create: (context) => PostBloc()..add(PostFetched()),
-        child: const PostBody(),
-      ),
+      body: const PostBody(),
     );
-  }
-}
-
-class PostBody extends StatefulWidget {
-  const PostBody({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<PostBody> createState() => _PostBodyState();
-}
-
-class _PostBodyState extends State<PostBody> {
-  final ScrollController _scrollController = ScrollController();
-  late PostBloc _postBloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _postBloc = context.read<PostBloc>();
-    _scrollController.addListener(_onScroll);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<PostBloc, PostState>(
-      builder: (context, state) {
-        // print(state);
-
-        if (state is PostInitial) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        // if Post is Loading
-        if (state is PostLoaded) {
-          if (state.posts.isEmpty) {
-            return const Text('No Data');
-          }
-
-          return RefreshIndicator(
-            onRefresh: _onRefresh,
-            child: PostList(
-              scrollController: _scrollController,
-              state: state,
-            ),
-          );
-        }
-
-        // if Post is Error
-
-        return const Center(
-          child: Text('Error Fetched Posts'),
-        );
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  void _onScroll() {
-    double maxScroll = _scrollController.position.maxScrollExtent;
-    double currentScroll = _scrollController.position.pixels;
-
-    if (currentScroll == maxScroll) _postBloc.add(PostFetched());
-  }
-
-  Future<void> _onRefresh() async {
-    _postBloc..add(PostRefresh());
   }
 }
